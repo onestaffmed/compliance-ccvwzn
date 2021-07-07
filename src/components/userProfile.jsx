f476f7552a6a141cf51d003b94b76613adb8b3a7
Samantha <shatfield@onestaffmedical.com>
2021-07-07 15:19:09 -0500

    Created profile edit option, started to flesh out edit content functionality.

--- src/components/userProfile.jsx
+++ src/components/userProfile.jsx
@@ -1,12 +1,12 @@
 import { IonCard, IonCardTitle, IonCardContent, IonCardHeader, IonAvatar, IonGrid, IonRow, IonCol, IonItemDivider, IonHeader, IonToolbar } from "@ionic/react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSquarePen } from "@fortawesome/free-solid-svg-icons";
+import { faCoffee, faPenSquare } from '@fortawesome/free-solid-svg-icons'
 import { create, } from 'ionicons/icons';
 
 const UserProfile = () => {
 
     return (
-        <div >
+        <div className="profileContainer">
 
 
             <IonToolbar>
@@ -22,13 +22,15 @@ const UserProfile = () => {
                                 <IonAvatar >
                                     <img alt="user" src="https://cdn.landesa.org/wp-content/uploads/default-user-image.png" />
                                 </IonAvatar>
-                                <IonCardTitle>FirstName LastName <ion-icon className="profileEditIcon" icon={create} /></IonCardTitle>
+                                <IonCardTitle>FirstName LastName  <div className="profileEditIcon"><FontAwesomeIcon icon={faPenSquare} color="orange" /></div></IonCardTitle>
+
                             </IonCardHeader>
 
                             <IonCardContent>
 
                                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse voluptate dolore at! Aliquid architecto nobis, recusandae minima, aperiam optio et blanditiis repudiandae, deleniti soluta suscipit. Eveniet voluptate perspiciatis voluptates unde.</p>
-                                <FontAwesomeIcon icon={['fas', 'faSquarePen']} />
+
+
 
                             </IonCardContent>
                         </IonCard>
@@ -36,14 +38,16 @@ const UserProfile = () => {
                             <IonCardHeader>
                                 <IonCardTitle>
                                     Personal Information
+                                    <div className="profileEditIcon"><FontAwesomeIcon icon={faPenSquare} color="orange" /></div>
                                 </IonCardTitle>
+                                {/* <div className="profileEditIcon"><FontAwesomeIcon icon={faPenSquare} color="orange" /></div> */}
                             </IonCardHeader>
 
                             <IonCardContent>
 
                                 <p>Address</p>
                                 <p>Phone</p>
-                                <ion-icon className="profileEditIcon" icon={create} />
+
                             </IonCardContent>
 
                         </IonCard>
@@ -51,6 +55,7 @@ const UserProfile = () => {
                             <IonCardHeader>
                                 <IonCardTitle>
                                     References
+                                    <div className="profileEditIcon"><FontAwesomeIcon icon={faPenSquare} color="orange" /></div>
                                 </IonCardTitle>
                             </IonCardHeader>
                             <IonCardContent>
@@ -69,9 +74,21 @@ const UserProfile = () => {
                                 <p>Address</p>
                                 <p>Phone</p>
                                 <p>Email</p>
-                                <ion-icon className="profileEditIcon" icon={create} />
+                                {/* <div className="profileEditIcon"><FontAwesomeIcon icon={faPenSquare} color="orange" /></div> */}
                             </IonCardContent>
 
+                        </IonCard>
+                        <IonCard className="cardTransparent">
+                            <IonCardHeader>
+                                <IonCardTitle>
+                                    Work Preferences
+                                    <div className="profileEditIcon"><FontAwesomeIcon icon={faPenSquare} color="orange" /></div>
+                                </IonCardTitle>
+                            </IonCardHeader>
+                            <IonCardContent>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, cupiditate! Odio ipsum aperiam, similique excepturi quae impedit molestias nostrum beatae ea voluptas numquam dignissimos, praesentium dicta provident asperiores harum autem!</p>
+
+                            </IonCardContent>
                         </IonCard>
                     </IonCol>
 
@@ -80,68 +97,77 @@ const UserProfile = () => {
                             <IonCardHeader>
                                 <IonCardTitle>
                                     Licenses
+                                    <div className="profileEditIcon"><FontAwesomeIcon icon={faPenSquare} color="orange" /></div>
                                 </IonCardTitle>
                             </IonCardHeader>
                             <IonCardContent>
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus enim minima consequatur unde perspiciatis maiores ea! Quae enim hic doloremque sed exercitationem, eius, reprehenderit aliquam doloribus repellat, iste molestias iusto.</p>
-                                <ion-icon className="profileEditIcon" icon={create} />
+
                             </IonCardContent>
                         </IonCard>
-                        <IonItemDivider></IonItemDivider>
+                        <IonItemDivider color="secondary"></IonItemDivider>
+                        <IonCard className="cardTransparent">
+                            <IonCardHeader>
+                                <IonCardTitle>
+                                    Education
+                                    <div className="profileEditIcon"><FontAwesomeIcon icon={faPenSquare} color="orange" /></div>
+                                </IonCardTitle>
+                            </IonCardHeader>
+                            <IonCardContent>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, cupiditate! Odio ipsum aperiam, similique excepturi quae impedit molestias nostrum beatae ea voluptas numquam dignissimos, praesentium dicta provident asperiores harum autem!</p>
+
+                            </IonCardContent>
+                        </IonCard>
+
                         <IonCard className="cardTransparent">
                             <IonCardHeader>
                                 <IonCardTitle>
                                     Skills
+                                    <div className="profileEditIcon"><FontAwesomeIcon icon={faPenSquare} color="orange" /></div>
                                 </IonCardTitle>
                             </IonCardHeader>
                             <IonCardContent>
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique eaque pariatur blanditiis quidem dolore deleniti quo sapiente ad tempore, inventore omnis cupiditate delectus ipsa laudantium magni eum, accusantium rem maxime?</p>
-                                <ion-icon className="profileEditIcon" icon={create} />
+
                             </IonCardContent>
                         </IonCard>
                         <IonCard className="cardTransparent">
                             <IonCardHeader>
                                 <IonCardTitle>
                                     Certifications
+                                    <div className="profileEditIcon"><FontAwesomeIcon icon={faPenSquare} color="orange" /></div>
                                 </IonCardTitle>
                             </IonCardHeader>
                             <IonCardContent>
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis nemo quod placeat nisi in facilis repellendus voluptatum nihil voluptates hic quisquam, praesentium debitis, esse aut minus doloribus quibusdam consequatur officia!</p>
-                                <ion-icon className="profileEditIcon" icon={create} />
+
                             </IonCardContent>
                         </IonCard>
-                        <IonItemDivider></IonItemDivider>
-                        <IonCard className="cardTransparent">
-                            <IonCardHeader>
-                                <IonCardTitle>
-                                    Education
-                                </IonCardTitle>
-                            </IonCardHeader>
-                            <IonCardContent>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, cupiditate! Odio ipsum aperiam, similique excepturi quae impedit molestias nostrum beatae ea voluptas numquam dignissimos, praesentium dicta provident asperiores harum autem!</p>
-                                <ion-icon className="profileEditIcon" icon={create} />
-                            </IonCardContent>
-                        </IonCard>
+
+
+                        <IonItemDivider color="primary"></IonItemDivider>
                         <IonCard className="cardTransparent">
                             <IonCardHeader>
                                 <IonCardTitle>
                                     Work History
+                                    <div className="profileEditIcon"><FontAwesomeIcon icon={faPenSquare} color="orange" /></div>
                                 </IonCardTitle>
                             </IonCardHeader>
                             <IonCardContent>
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima odit ut culpa aperiam qui iste corrupti exercitationem quam ipsum sunt iusto soluta dicta nulla, praesentium placeat voluptates ratione unde dolore!</p>
-                                <ion-icon className="profileEditIcon" icon={create} />
+
                             </IonCardContent>
                         </IonCard>
                         <IonCard className="cardTransparent">
                             <IonCardHeader>
                                 <IonCardTitle>
                                     Medical History
+                                    <div className="profileEditIcon"><FontAwesomeIcon icon={faPenSquare} color="orange" /></div>
                                 </IonCardTitle>
                             </IonCardHeader>
                             <IonCardContent>
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam commodi nam blanditiis ducimus expedita? Animi, libero facere sed quos odit repudiandae voluptatum sequi cum est maxime, qui explicabo, architecto incidunt.</p>
-                                <ion-icon className="profileEditIcon" icon={create} />
+
                             </IonCardContent>
                         </IonCard>
                     </IonCol>
