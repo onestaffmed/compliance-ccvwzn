70ff35d85711b65694bb1ef23f4281382390c57a
Samantha <samantha@justdreamwild.com>
2021-07-15 10:32:37 -0500

    Database update

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,18 +5,26 @@ const profileUser = `../profile.json`;
 export const getUserProfile =() => {
     return axios.get(profileUser);
 };
-
+//** Mock Server API
 // const userLicenses = `http://345vy.mocklab.io/licenses`;
+
+// ** Local Dev API
 // const userLicenses = ` http://localhost:3000/licenses`;
+
+// ** Demo API
 const userLicenses = ` http://10.0.0.127:3000/licenses`;
 
 export const getAllLicenses =() => {
     return axios.get(userLicenses);
     
 };
-
+//** Mock Server API
 // const userEducation = `http://345vy.mocklab.io/education`;
+
+// ** Local Dev API
 // const userEducation = `http://localhost:3000/education`;
+
+// ** Demo API
 const userEducation = `http://10.0.0.127:3000/education`;
 
 
@@ -24,9 +32,13 @@ export const getAllEdu =() => {
     return axios.get(userEducation);
     
 };
-
+//** Mock Server API
 // const userCerts = `http://345vy.mocklab.io/certifications`;
-// const userCerts = `http://localhost:3000/certifications`;
+
+// ** Local Dev API
+// const userCerts = `http://localhost:3000/certifications`;\
+
+// ** Demo API
 const userCerts = `http://10.0.0.127:3000/certifications`;
 export const getAllCerts =() => {
     return axios.get(userCerts);
